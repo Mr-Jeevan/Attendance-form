d40830170626ac6e4f25ea5cc667887427534531
Mr-Jeevan <jeevee.a77@gmail.com>
2025-08-07 15:02:22 +0530

    just nothing

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,40 @@ import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
 import LoginPage from './pages/Login/LoginPage.jsx';
 import McaOne from './pages/McaOne.jsx';
 
+
 // ⛔ Utility: Redirect to prevent back navigation
 const PreventBackNav = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+	const location = useLocation();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (location.pathname === '/mca') {
-      window.history.pushState(null, '', window.location.href);
-      const onPopState = () => {
-        navigate('/mca', { replace: true });
-      };
-      window.addEventListener('popstate', onPopState);
-      return () => {
-        window.removeEventListener('popstate', onPopState);
-      };
-    }
-  }, [location, navigate]);
+	useEffect(() => {
+		if (location.pathname === '/mca') {
+			window.history.pushState(null, '', window.location.href);
+			const onPopState = () => {
+				navigate('/mca', { replace: true });
+			};
+			window.addEventListener('popstate', onPopState);
+			return () => {
+				window.removeEventListener('popstate', onPopState);
+			};
+		}
+	}, [location, navigate]);
 
-  return null;
+	return null;
 };
 
 function App() {
-  return (
-    <Router>
-      <PreventBackNav />
-      <Routes>
-        <Route path="/" element={<FacialAttendanceLanding />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/mca" element={<McaOne />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<PreventBackNav />
+			<Routes>
+				<Route path="/" element={<FacialAttendanceLanding />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/mca" element={<McaOne />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
